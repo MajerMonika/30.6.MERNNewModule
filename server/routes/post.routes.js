744c516c1506211f68ac6a104a042ff5a6e65b1f
Monika Majer <majer_monika@interia.pl>
2019-01-01 14:00:07 +0100

    Copy files from 30.5.

--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -11,7 +11,13 @@ router.route('/posts/:cuid').get(PostController.getPost);
 // Add a new Post
 router.route('/posts').post(PostController.addPost);
 
+// Update a post by cuid
+router.route('/posts/:cuid').put(PostController.editPost);
+
 // Delete a post by cuid
 router.route('/posts/:cuid').delete(PostController.deletePost);
+
+router.route("/posts/:cuid/up").put(PostController.thumbUp);
+router.route("/posts/:cuid/down").put(PostController.thumbDown);
 
 export default router;
